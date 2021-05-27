d317e26cb63bf41c74862c4a4fb8ad3a48a5a989
nmenag <nmena.garzon@gmail.com>
2021-05-27 18:58:24 -0500

    Update packages
     - Bump version
     - Update latest version dependencies

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Zookeeper.Mixfile do
   def project do
     [
       app: :zookeeper,
-      version: "0.0.1",
-      elixir: ">= 1.0.0",
+      version: "0.1.1",
+      elixir: "~>1.10",
       deps: deps(),
       package: package(),
       description: "Zookeeper client for Elixir with common recipes."
@@ -20,9 +20,9 @@ defmodule Zookeeper.Mixfile do
 
   defp deps do
     [
-      {:erlzk, github: "huaban/erlzk", ref: "a005aab956e88686d8d9e719cb3937ae91c98b68"},
-      {:dialyze, ">= 0.2.0", only: :dev},
-      {:uuid, "~> 1.1"},
+      {:erlzk, "~> 0.6.4"},
+      {:dialyze, "~> 0.2.1",  only: :dev},
+      {:uuid, "~> 1.1"}
     ]
   end
 
