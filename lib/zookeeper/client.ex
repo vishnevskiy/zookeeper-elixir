407ceaba2f9c0aa392b6dbdc35ee7690a0ff1a32
josuedani <josuepirir@kuak.me>
2021-05-28 11:41:15 -0600

    Handled error on start
    ADD validation when zookeeper is not available on start.

--- lib/zookeeper/client.ex
+++ lib/zookeeper/client.ex
@@ -4,27 +4,21 @@ defmodule Zookeeper.Client do
   alias Zookeeper.ZnodeStat
 
   @type host :: {char_list, integer}
-  @type hosts :: String.t() | [host]
-  @type options :: [
-          timeout: integer,
-          chroot: char_list,
-          disable_watch_auto_reset: boolean,
-          auth_data: [{char_list, binary}],
-          stop_on_disconnect: boolean
-        ]
+  @type hosts :: String.t | [host]
+  @type options :: [timeout: integer,
+                    chroot: char_list,
+                    disable_watch_auto_reset: boolean,
+                    auth_data: [{char_list, binary}],
+                    stop_on_disconnect: boolean]
   @type acl :: {[perms], scheme, id}
   @type perms :: :r | :w | :c | :d | :a
   @type scheme :: char_list
   @type id :: char_list
   @type create_mode :: :persistent | :ephemeral | :persistent_sequential | :ephemeral_sequential
-  @type create_error ::
-          :node_exists
-          | :no_node
-          | :no_auth
-          | :invalid_acl
-          | :no_children_for_ephemerals
-          | :closed
-  @type create_options :: [create_mode: create_mode, acl: [acl], makepath: boolean]
+  @type create_error :: :node_exists | :no_node | :no_auth | :invalid_acl | :no_children_for_ephemerals | :closed
+  @type create_options :: [create_mode: create_mode,
+                           acl: [acl],
+                           makepath: boolean]
 
   # This Id represents anyone.
   @zk_id_anyone_id_unsafe ['world', 'anyone']
@@ -32,11 +26,11 @@ defmodule Zookeeper.Client do
   @zk_id_auth_ids ['auth', '']
 
   # This is a completely open ACL.
-  @zk_acl_open_acl_unsafe [:rwcda | @zk_id_anyone_id_unsafe] |> List.to_tuple()
+  @zk_acl_open_acl_unsafe [:rwcda|@zk_id_anyone_id_unsafe] |> List.to_tuple
   # This ACL gives the creators authentication id's all permissions.
-  @zk_acl_creator_all_acl [:rwcda | @zk_id_auth_ids] |> List.to_tuple()
+  @zk_acl_creator_all_acl [:rwcda|@zk_id_auth_ids] |> List.to_tuple
   # This ACL gives the world the ability to read.
-  @zk_acl_read_acl_unsafe [:r | @zk_id_anyone_id_unsafe] |> List.to_tuple()
+  @zk_acl_read_acl_unsafe [:r|@zk_id_anyone_id_unsafe] |> List.to_tuple
 
   @doc """
   Connect to ZooKeeper.
@@ -69,21 +63,18 @@ defmodule Zookeeper.Client do
   @doc """
   Create a node with all possible options.
   """
-  @spec create(pid, String.t(), binary, create_options) ::
-          {:ok, String.t()} | {:error, create_error}
+  @spec create(pid, String.t, binary, create_options) :: {:ok, String.t} | {:error, create_error}
   def create(pid, path, value \\ "", opts \\ []) do
     GenServer.call(pid, {:create, path, value, opts})
   end
 
-  @spec create!(pid, String.t(), binary, create_options) ::
-          {:ok, String.t()} | {:error, create_error}
+  @spec create!(pid, String.t, binary, create_options) :: {:ok, String.t} | {:error, create_error}
   def create!(pid, path, value \\ "", opts \\ []), do: apply!(&create/4, [pid, path, value, opts])
 
   @doc """
   Recursively create a path if it doesn’t exist.
   """
-  @spec ensure_path(pid, String.t(), [acl] | nil) ::
-          :ok | {:error, :no_auth | :invalid_acl | :no_children_for_ephemerals | :closed}
+  @spec ensure_path(pid, String.t, [acl] | nil) :: :ok | {:error, :no_auth | :invalid_acl | :no_children_for_ephemerals | :closed}
   def ensure_path(pid, path, acl \\ nil) do
     GenServer.call(pid, {:ensure_path, path, acl})
   end
@@ -91,43 +82,40 @@ defmodule Zookeeper.Client do
   @doc """
   Check if a node exists.
   """
-  @spec exists(pid, String.t(), pid | nil) :: {:ok, %ZnodeStat{}} | {:error, :no_node | :closed}
+  @spec exists(pid, String.t, pid | nil) :: {:ok, %ZnodeStat{}} | {:error, :no_node | :closed}
   def exists(pid, path, watcher \\ nil) do
     GenServer.call(pid, {:exists, path, watcher})
   end
 
-  @spec exists!(pid, String.t(), pid | nil) :: %ZnodeStat{}
+  @spec exists!(pid, String.t, pid | nil) :: %ZnodeStat{}
   def exists!(pid, path, watch \\ nil), do: apply!(&exists/3, [pid, path, watch])
 
   @doc """
   Get the value of a node.
   """
-  @spec get(pid, String.t(), pid | nil) ::
-          {:ok, {binary, %ZnodeStat{}}} | {:error, :no_node | :no_auth | :closed}
+  @spec get(pid, String.t, pid | nil) :: {:ok, {binary, %ZnodeStat{}}} | {:error, :no_node | :no_auth | :closed}
   def get(pid, path, watcher \\ nil) do
     GenServer.call(pid, {:get, path, watcher})
   end
 
-  @spec get!(pid, String.t(), pid | nil) :: {binary, %ZnodeStat{}}
+  @spec get!(pid, String.t, pid | nil) :: {binary, %ZnodeStat{}}
   def get!(pid, path, watcher \\ nil), do: apply!(&get/3, [pid, path, watcher])
 
   @doc """
   Get a list of child nodes of a path.
   """
-  @spec get_children(pid, String.t(), pid | nil) ::
-          {:ok, [String.t()]} | {:error, :no_node | :no_auth | :closed}
+  @spec get_children(pid, String.t, pid | nil) :: {:ok, [String.t]} | {:error, :no_node | :no_auth | :closed}
   def get_children(pid, path, watcher \\ nil) do
     GenServer.call(pid, {:get_children, path, watcher})
   end
 
-  @spec get_children!(pid, String.t(), pid | nil) :: [String.t()]
+  @spec get_children!(pid, String.t, pid | nil) :: [String.t]
   def get_children!(pid, path, watcher \\ nil), do: apply!(&get_children/3, [pid, path, watcher])
 
   @doc """
   Return the ACL and stat of the node of the given path.
   """
-  @spec get_acls(pid, String.t()) ::
-          {:ok, {[acl], %ZnodeStat{}}} | {:error, :no_node | :no_auth | :closed}
+  @spec get_acls(pid, String.t) :: {:ok, {[acl], %ZnodeStat{}}} | {:error, :no_node | :no_auth | :closed}
   def get_acls(pid, path) do
     GenServer.call(pid, {:pid, path})
   end
@@ -135,9 +123,7 @@ defmodule Zookeeper.Client do
   @doc """
   Set the ACL for the node of the given path.
   """
-  @spec set_acls(pid, String.t(), [acl], integer) ::
-          {:ok, %ZnodeStat{}}
-          | {:error, :no_node | :bad_version | :no_auth | :closed | :invalid_acl}
+  @spec set_acls(pid, String.t, [acl], integer) :: {:ok, %ZnodeStat{}} | {:error, :no_node | :bad_version | :no_auth | :closed | :invalid_acl}
   def set_acls(pid, path, acls, version \\ -1) do
     GenServer.call(pid, {:set_acls, path, acls, version})
   end
@@ -145,37 +131,37 @@ defmodule Zookeeper.Client do
   @doc """
   Set the value of a node.
   """
-  @spec set(pid, String.t(), binary, integer) ::
-          {:ok, %ZnodeStat{}} | {:error, :no_node | :bad_version | :no_auth | :closed}
+  @spec set(pid, String.t, binary, integer) :: {:ok, %ZnodeStat{}} | {:error, :no_node | :bad_version | :no_auth | :closed}
   def set(pid, path, value, version \\ -1) do
     GenServer.call(pid, {:set, path, value, version})
   end
 
-  @spec set!(pid, String.t(), binary, integer) :: %ZnodeStat{}
+  @spec set!(pid, String.t, binary, integer) :: %ZnodeStat{}
   def set!(pid, path, value, version \\ -1), do: apply!(&set/4, [pid, path, value, version])
 
   @doc """
   Delete a node.
   """
-  @spec delete(pid, String.t(), integer, boolean) ::
-          :ok | {:error, :no_node | :bad_version | :no_auth | :closed | :not_empty}
+  @spec delete(pid, String.t, integer, boolean) :: :ok | {:error, :no_node | :bad_version | :no_auth | :closed | :not_empty}
   def delete(pid, path, version \\ -1, recursive \\ false) do
     GenServer.call(pid, {:delete, path, version, recursive})
   end
 
-  @spec delete!(pid, String.t(), integer, boolean) :: :ok
-  def delete!(pid, path, version \\ -1, recursive \\ false),
-    do: apply!(&delete/4, [pid, path, version, recursive])
+  @spec delete!(pid, String.t, integer, boolean) :: :ok
+  def delete!(pid, path, version \\ -1, recursive \\ false), do: apply!(&delete/4, [pid, path, version, recursive])
 
   ## Server
 
+  @spec init({binary | [{list, integer}], any, any}) ::
+          {:ok, %{stop_on_disconnect: any, watchers: %{}, zk: pid}}
   def init({hosts, timeout, stop_on_disconnect}) do
-    {:ok, pid} =
-      hosts
-      |> parse_hosts
-      |> :erlzk_conn.start_link(timeout, monitor: self())
 
-    {:ok, %{zk: pid, watchers: %{}, stop_on_disconnect: stop_on_disconnect}}
+    case hosts
+        |> parse_hosts
+        |> :erlzk_conn.start_link(timeout, monitor: self()) do
+          {:ok, pid} -> {:ok, %{zk: pid, watchers: %{}, stop_on_disconnect: stop_on_disconnect}}
+          {:error, :no_available_server} -> {:error, :no_available_server}
+    end
   end
 
   def terminate(_reason, %{zk: zk}) do
@@ -186,15 +172,15 @@ defmodule Zookeeper.Client do
     {:stop, :normal, :ok, state}
   end
 
-  def handle_call({:create, path, value, opts}, _from, %{zk: zk} = state) do
+  def handle_call({:create, path, value, opts}, _from, %{zk: zk}=state) do
     {:reply, do_create(zk, path, value, opts), state}
   end
 
-  def handle_call({:ensure_path, path, acl}, _from, %{zk: zk} = state) do
+  def handle_call({:ensure_path, path, acl}, _from, %{zk: zk}=state) do
     {:reply, do_ensure_path(zk, path, acl), state}
   end
 
-  def handle_call({:exists, path, watcher}, _from, %{zk: zk} = state) do
+  def handle_call({:exists, path, watcher}, _from, %{zk: zk}=state) do
     if watcher == nil do
       {:reply, do_exists(zk, path, false), state}
     else
@@ -204,7 +190,7 @@ defmodule Zookeeper.Client do
     end
   end
 
-  def handle_call({:get, path, watcher}, _from, %{zk: zk, watchers: watchers} = state) do
+  def handle_call({:get, path, watcher}, _from, %{zk: zk, watchers: watchers}=state) do
     if watcher == nil do
       {:reply, do_get(zk, path, false), state}
     else
@@ -214,7 +200,7 @@ defmodule Zookeeper.Client do
     end
   end
 
-  def handle_call({:get_children, path, watcher}, _from, %{zk: zk} = state) do
+  def handle_call({:get_children, path, watcher}, _from, %{zk: zk}=state) do
     if watcher == nil do
       {:reply, do_get_children(zk, path, false), state}
     else
@@ -224,19 +210,19 @@ defmodule Zookeeper.Client do
     end
   end
 
-  def handle_call({:get_acls, path}, _from, %{zk: zk} = state) do
+  def handle_call({:get_acls, path}, _from, %{zk: zk}=state) do
     {:reply, do_get_acls(zk, path), state}
   end
 
-  def handle_call({:set_acls, path, acl, version}, _from, %{zk: zk} = state) do
+  def handle_call({:set_acls, path, acl, version}, _from, %{zk: zk}=state) do
     {:reply, do_set_acls(zk, path, acl, version), state}
   end
 
-  def handle_call({:set, path, value, version}, _from, %{zk: zk} = state) do
+  def handle_call({:set, path, value, version}, _from, %{zk: zk}=state) do
     {:reply, do_set(zk, path, value, version), state}
   end
 
-  def handle_call({:delete, path, version, recursive}, _from, %{zk: zk} = state) do
+  def handle_call({:delete, path, version, recursive}, _from, %{zk: zk}=state) do
     {:reply, do_delete(zk, path, version, recursive), state}
   end
 
@@ -256,7 +242,7 @@ defmodule Zookeeper.Client do
     {:noreply, state}
   end
 
-  def handle_info({:disconnected, _host, _port}, %{stop_on_disconnect: true} = state) do
+  def handle_info({:disconnected, _host, _port}, %{stop_on_disconnect: true}=state) do
     {:stop, :disconnected, state}
   end
 
@@ -276,7 +262,6 @@ defmodule Zookeeper.Client do
         nil -> [@zk_acl_open_acl_unsafe]
         acl -> acl
       end
-
     create_mode = Keyword.get(opts, :create_mode, :persistent)
     makepath = Keyword.get(opts, :makepath, false)
 
@@ -292,20 +277,14 @@ defmodule Zookeeper.Client do
 
   def do_ensure_path(pid, path, acl \\ nil) do
     case do_create(pid, path, "", acl: acl) do
-      {:ok, _} ->
-        :ok
-
-      {:error, :node_exists} ->
-        :ok
-
+      {:ok, _} -> :ok
+      {:error, :node_exists} -> :ok
       {:error, :no_node} ->
         case do_ensure_path(pid, Path.dirname(path), acl) do
           :ok -> do_ensure_path(pid, path, acl)
           error -> error
         end
-
-      error ->
-        error
+      error -> error
     end
   end
 
@@ -316,7 +295,6 @@ defmodule Zookeeper.Client do
       else
         :erlzk_conn.exists(pid, normalize_path(path), false)
       end
-
     case reply do
       {:ok, stat} -> {:ok, ZnodeStat.new(stat)}
       error -> error
@@ -330,7 +308,6 @@ defmodule Zookeeper.Client do
       else
         :erlzk_conn.get_data(pid, normalize_path(path), false)
       end
-
     case reply do
       {:ok, {value, stat}} -> {:ok, {value, ZnodeStat.new(stat)}}
       error -> error
@@ -344,7 +321,6 @@ defmodule Zookeeper.Client do
       else
         :erlzk_conn.get_children(pid, normalize_path(path), false)
       end
-
     case reply do
       {:ok, children} -> {:ok, children |> Enum.map(&to_string/1)}
       error -> error
@@ -359,7 +335,7 @@ defmodule Zookeeper.Client do
   end
 
   def do_set_acls(pid, path, acls, version \\ -1) do
-    case :erlzk_conn.set_acl(pid, normalize_path(path), acls, version) do
+     case :erlzk_conn.set_acl(pid, normalize_path(path), acls, version) do
       {:ok, stat} -> {:ok, ZnodeStat.new(stat)}
       error -> error
     end
@@ -378,12 +354,8 @@ defmodule Zookeeper.Client do
         {:ok, children} ->
           children |> Enum.each(&do_delete(pid, Path.join(path, &1), -1, true))
           do_delete(pid, path, version)
-
-        {:error, :no_node} ->
-          :ok
-
-        error ->
-          error
+        {:error, :no_node} -> :ok
+        error -> error
       end
     else
       :erlzk_conn.delete(pid, normalize_path(path), version)
@@ -392,19 +364,17 @@ defmodule Zookeeper.Client do
 
   ## Private
 
-  @spec normalize_path(String.t() | char_list) :: char_list
-  defp normalize_path(path) when is_bitstring(path),
-    do: path |> String.to_char_list() |> normalize_path
+  @spec normalize_path(String.t | char_list) :: char_list
+  defp normalize_path(path) when is_bitstring(path), do: path |> String.to_char_list |> normalize_path
+  defp normalize_path([?/|_]=path), do: path
+  defp normalize_path(path), do: [?/|path]
 
-  defp normalize_path([?/ | _] = path), do: path
-  defp normalize_path(path), do: [?/ | path]
-
-  @spec parse_hosts(String.t()) :: [host]
+  @spec parse_hosts(String.t) :: [host]
   defp parse_hosts(hosts) when is_bitstring(hosts) do
     hosts
     |> String.split(",")
     |> Enum.map(&String.split(&1, ":"))
-    |> Enum.map(fn [host, port] -> {String.to_char_list(host), String.to_integer(port)} end)
+    |> Enum.map(fn([host, port]) -> {String.to_char_list(host), String.to_integer(port)} end)
   end
 
   @spec parse_hosts([host]) :: [host]
@@ -422,11 +392,9 @@ defmodule Zookeeper.Client do
   @spec notify_watchers(map(), :exists | :data | :children, char_list) :: map()
   defp notify_watchers(watchers, type, path) do
     path = path |> to_string
-
     case Map.pop(watchers, {type, path}) do
       {nil, watchers} ->
         watchers
-
       {receivers, watchers} ->
         receivers |> Enum.each(&send(&1, {__MODULE__, path, type}))
         watchers
@@ -436,7 +404,7 @@ defmodule Zookeeper.Client do
   @spec notify_watchers(map(), {:exists | :data | :children, char_list}, pid) :: map()
   defp update_watchers(watchers, key, watcher) do
     if Map.has_key?(watchers, key) do
-      watchers |> Map.put(key, [watcher | Map.get(watchers, key)])
+      watchers |> Map.put(key, [watcher|Map.get(watchers, key)])
     else
       watchers |> Map.put_new(key, [watcher])
     end
